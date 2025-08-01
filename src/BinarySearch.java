28800d54b02e2f36527ea943375998bb4c608986
Easwar <host57203@gmail.com>
2025-08-01 05:32:31 +0530

    The first dsa program

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,3 +1,22 @@
 public class BinarySearch {
+    public static void main(String[] args){
+        int[] arr = {1,3,5,7,13,24,37,44,59};
+        System.out.println(binarySearch(arr,5));
+    }
+    private static int binarySearch(int[] nums,int target){
+        int start = 0;
+        int end = nums.length-1;
 
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(target < nums[mid]){
+                end = mid-1;
+            }else if(target > nums[mid]){
+                start = mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
 }
